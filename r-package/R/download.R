4b99e4e3de2cf367ff4dd0673845ca343f7a62ad
Arthur Lins de Vasconcellos <tuca1211@gmail.com>
2023-02-28 14:29:53 -0300

    Updating the download.R file
    Updating the download.R file in order to support the new file-naming function, "select_quarter".

--- r-package/R/download.R
+++ r-package/R/download.R
@@ -1,5 +1,6 @@
-
+source("~/GitHub/PNAD_Continua/r-package/R/select_file_function.R")
 download_quarter <- function(quarter, year, directory = getwd()) {
+
   if (!dir.exists(directory)) {
     stop("Provided directory doesn't exist")
   }
@@ -7,28 +8,14 @@ download_quarter <- function(quarter, year, directory = getwd()) {
     stop("Provided period is not valid")
   }
 
-
-  if (year >= 2019 & quarter >= 2 | year >= 2020) {
-    quarter <- stringr::str_pad(quarter,
-      width = 2,
-      side = "left",
-      pad = 0
-    )
-    file_name <- paste0("PNADC_", quarter, year, ".zip")
-  } else {
-    quarter <- stringr::str_pad(quarter,
-      width = 2,
-      side = "left",
-      pad = 0
-    )
-
-    #### Ficar de olho para ver se o IBGE muda o seu padrão de nomes nos códigos
-    file_name <- paste0("PNADC_", quarter, year, "_20220916.zip")
-  }
+#pasting the file's name
+  file_name.intermediary <- paste(select_quarter(year,quarter))
+  x<- paste0(file_name.intermediary, ".zip")
+#adding the year before
+  file_name<-paste0(year,"/",x)
 
   url_path <- file.path(
     "http://ftp.ibge.gov.br/Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_continua/Trimestral/Microdados",
-    year,
     file_name
   )
 
@@ -37,6 +24,12 @@ utils::download.file(
     destfile = file.path(directory, file_name),
     mode = "wb"
   )
+
+# Check if the "PNADC_microdata" directory exists and create it if it doesn't
+pnadc_dir <- file.path(directory, "PNADC_microdata")
+if (!dir.exists(pnadc_dir)) {
+  dir.create(pnadc_dir, recursive = TRUE)
+}
 
   utils::unzip(
     zipfile = file.path(directory, file_name),
