85f86789b2fa9d59bccff79a329d880c0fd564f6
Arthur Lins de Vasconcellos <tuca1211@gmail.com>
2023-03-05 00:21:45 -0300

    Deleting the old function's code and making changes on the new code
    I had kept the old function's code, for now, as i like to build my new cod always checking for the old one, this commit should be considered an ordinary one, more as a work routine specifically for me.
    I also made significant change son the new code, adding a bigger timeout for downloading the file, in order to handle slower connections, I added a old function at the end that I had somewhat forgotten to add when I deleted the old code. I added code chunks that put the file in a subfolder inside "PNADC_Microdata" with the name of the year of that PNAD and another one that takes the downloaded file and generates an unzipped version at the same folder.

--- r-package/R/download.R
+++ r-package/R/download.R
@@ -1,47 +1,6 @@
 source("~/GitHub/PNAD_Continua/r-package/R/select_file_function.R")
-download_quarter <- function(quarter, year, directory = "~") {
 
-  if (!dir.exists(directory)) {
-    stop("Provided directory doesn't exist")
-  }
-  if (!is_period_valid(quarter, year)) {
-    stop("Provided period is not valid")
-  }
-
-#pasting the file's name
-  file_name.intermediary <- paste(select_quarter(year,quarter))
-  x<- paste0(file_name.intermediary,".zip")
-#adding the year before
-  file_name<-paste0(year,"/",x)
-
-  url_path <- file.path(
-    "http://ftp.ibge.gov.br/Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_continua/Trimestral/Microdados/",
-    file_name
-  )
-
-  # Check if the "PNADC_microdata" directory exists and create it if it doesn't
-  pnadc_dir <- file.path(directory, "PNADC_microdata")
-  if (!dir.exists(pnadc_dir)) {
-    dir.create(pnadc_dir, recursive = TRUE)
-  }
-
-filename<-basename(url_path)
-dest_path <- file.path(directory, filename)
-
-utils::download.file(
-    url = url_path,
-    destfile = file.path(pnadc_dir,file_name),
-    method = "auto"
-  )
-
-  return(file.path(directory, "PNADC_microdata"))
-}
-
-is_period_valid <- function(quarter, year) {
-  quarter >= 1 && quarter <= 4 && year >= 2012 && year <= timeDate::getRmetricsOptions("currentYear")
-}
-
-#Even newer version of ChatGPT because he wronged the last one
+#Even newer version, with ChatGPT corrections and comments (double revised by a human)
 
 download_quarter <- function(quarter, year, directory = "~") {
 
@@ -65,7 +24,12 @@ download_quarter <- function(quarter, year, directory = "~") {
   )
 
   # Check if the "PNADC_microdata" directory exists and create it if it doesn't
-  pnadc_dir <- file.path(directory, "PNADC_microdata")
+  pnadc_dir.1 <- file.path(directory, "PNADC_microdata")
+  if (!dir.exists(pnadc_dir.1)) {
+    dir.create(pnadc_dir.1, recursive = TRUE)
+  }
+  # Check if the "PNADC_microdata/year" directory exists and create it if it doesn't
+  pnadc_dir <- file.path(directory, "PNADC_microdata",year)
   if (!dir.exists(pnadc_dir)) {
     dir.create(pnadc_dir, recursive = TRUE)
   }
@@ -76,8 +40,16 @@ download_quarter <- function(quarter, year, directory = "~") {
   utils::download.file(
     url = url_path,
     destfile = dest_path,
-    method = "auto"
+    method = "auto",
+    timeout= 120
   )
 
+  # Unzip the file to the same directory
+  unzip(dest_path, exdir = pnadc_dir)
+
   return(pnadc_dir)
+}
+
+is_period_valid <- function(quarter, year) {
+  quarter >= 1 && quarter <= 4 && year >= 2012 && year <= timeDate::getRmetricsOptions("currentYear")
 }
