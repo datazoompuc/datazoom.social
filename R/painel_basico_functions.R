ca4e3bfe68df599508dddbf360c0b3a53884e79c
Laura T. Regadas <lauratregadas@gmail.com>
2023-04-17 11:36:16 -0300

    organizes intermediate objects and comments

--- R/painel_basico_functions.R
+++ R/painel_basico_functions.R
@@ -2,18 +2,14 @@
 library(tidyverse)
 #filtering the data base received by the user to not have year, month and day =99 or =9999
 
-pnad_tri1<- pnadjunta21_1_22_1 %>% filter(Ano == 2021, Trimestre== 1)
-pnad_tri2<- pnadjunta21_1_22_1 %>% filter(Ano == 2021, Trimestre== 2)
-pnad_tri3<- pnadjunta21_1_22_1 %>% filter(Ano == 2021, Trimestre== 3)
-pnad_tri4<- pnadjunta21_1_22_1 %>% filter(Ano == 2021, Trimestre== 4)
-pnad_tri5<- pnadjunta21_1_22_1 %>% filter(Ano == 2022, Trimestre== 1)
-
 filtra_dados= function(dados_recebidos){
-  dados_recebidos<-pnad_tri1
-  dados_recebidos = as.data.frame(dados_recebidos)
+  dados_raw = dados_recebidos %>%
+    as.data.frame()
   
-  dados_filtrados = dados_recebidos %>% dplyr::filter(V2008 != "99" | V20081!= "99" | V20082!= "9999")
+  #filtering the data base received by the user to not have year, month and day =99 or =9999
+  dados_filtrados = dados_raw %>% dplyr::filter(V2008 != "99" | V20081!= "99" | V20082!= "9999")
   
+  #considerar fazer com mutate_at, pode ser mais rápido
   dados_character = dados_filtrados %>%
     dplyr::mutate(V2007 = as.character(V2007)) %>%
     dplyr::mutate(V2008 = as.character(V2008)) %>%
@@ -21,11 +17,13 @@ filtra_dados= function(dados_recebidos){
     dplyr::mutate(V20082 = as.character(V20082))
   com_id_dom = dados_character %>%
     dplyr::bind_cols(
-      ### Adiciona identificadores tanto para municipios quanto chefe
-      
+      # adiciona identificadores tanto para domicilio
       id_dom = dplyr::group_indices(as.data.frame(dados_character),dados_character$UPA, dados_character$V1008, dados_character$V1014))
+  
+  #retorna produto final
   return(com_id_dom)
 }
-i<-filtra_dados(pnad_tri1)
+
+test_run = filtra_dados(pnad_tri1)
 
 
