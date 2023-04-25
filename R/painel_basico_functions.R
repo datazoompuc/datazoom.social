aad63ab46233f89681a88246bb977178e1497154
Laura T. Regadas <lauratregadas@gmail.com>
2023-04-25 09:39:48 -0300

    separates into two functions

--- R/painel_basico_functions.R
+++ R/painel_basico_functions.R
@@ -1,7 +1,7 @@
 #creating the basic panel 
 library(tidyverse)
 
-filters_data = function(incoming_dat){
+cleans_dat = function(incoming_dat){
   raw_dat = incoming_dat %>%
     as.data.frame()
   
@@ -16,15 +16,21 @@ filters_data = function(incoming_dat){
     dplyr::mutate(V2008 = as.character(V2008)) %>%
     dplyr::mutate(V20081 = as.character(V20081)) %>%
     dplyr::mutate(V20082 = as.character(V20082))
+
+  return(character_dat)
   
+}
+
+
+builds_identifiers = function(character_dat) {
   w_id_dom = character_dat %>%
     dplyr::bind_cols(
       # creates household identifier
-      id_dom = dplyr::group_indices(as.data.frame(dados_character), character_dat$UPA, character_dat$V1008, character_dat$V1014))
+      id_dom = dplyr::group_indices(as.data.frame(character_dat), character_dat$UPA, character_dat$V1008, character_dat$V1014))
   
   #creates individual identifier
-  #id_ind concatenates househol identifier (id_dom), birthday in Ymd format and gender
-  basic_panel = com_id_dom %>%
+  #id_ind concatenates household identifier (id_dom), birthday in Ymd format and gender
+  basic_panel = w_id_dom %>%
     dplyr::mutate(id_ind = paste(id_dom, V20082, V20081, V2008, V2007))
   
   #returns the final product
