003ea75ace2fba132df510997f499c7e73d26166
Laura T. Regadas <lauratregadas@gmail.com>
2023-04-25 09:36:35 -0300

    translates everything to english

--- R/painel_basico_functions.R
+++ R/painel_basico_functions.R
@@ -1,36 +1,33 @@
 #creating the basic panel 
 library(tidyverse)
 
-filtra_dados = function(dados_recebidos){
-  dados_raw = dados_recebidos %>%
+filters_data = function(incoming_dat){
+  raw_dat = incoming_dat %>%
     as.data.frame()
   
   #filtering the data base received by the user to not have year, month and day =99 or =9999
-  #necessario filtrar para V2007 != 99?
-  dados_filtrados = dados_raw %>% dplyr::filter(V2008 != "99" | V20081!= "99" | V20082!= "9999")
+  #filter V2007 != 99?
+  filtered_dat = raw_dat %>% dplyr::filter(V2008 != "99" | V20081!= "99" | V20082!= "9999")
   
-  #considerar fazer com mutate_at, pode ser mais rapido
-  #outra opcao e pular essa etapa e criar os identificadores com paste0()
-  dados_character = dados_filtrados %>%
+  #consider mutate_at, could be faster
+  #or simply create the identifier w/ paste0()
+  character_dat = filtered_dat %>%
     dplyr::mutate(V2007 = as.character(V2007)) %>%
     dplyr::mutate(V2008 = as.character(V2008)) %>%
     dplyr::mutate(V20081 = as.character(V20081)) %>%
     dplyr::mutate(V20082 = as.character(V20082))
   
-  com_id_dom = dados_character %>%
+  w_id_dom = character_dat %>%
     dplyr::bind_cols(
-      # adiciona identificadores tanto para domicilio
-      id_dom = dplyr::group_indices(as.data.frame(dados_character),dados_character$UPA, dados_character$V1008, dados_character$V1014))
+      # creates household identifier
+      id_dom = dplyr::group_indices(as.data.frame(dados_character), character_dat$UPA, character_dat$V1008, character_dat$V1014))
   
-  #cria identificador basico
-  #id_ind concatena identificador de domicilio, data de nascimento no formado Ymd e sexo
+  #creates individual identifier
+  #id_ind concatenates househol identifier (id_dom), birthday in Ymd format and gender
   basic_panel = com_id_dom %>%
     dplyr::mutate(id_ind = paste(id_dom, V20082, V20081, V2008, V2007))
   
-  #retorna produto final
+  #returns the final product
   return(basic_panel)
 }
-
-test_run = filtra_dados(pnad_tri1)
-
 
