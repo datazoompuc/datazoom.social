5670acb88764b9feab6112054251e5f7f515ff3d
Laura T. Regadas <lauratregadas@gmail.com>
2023-05-16 17:36:34 -0300

    loop working!

--- R/Testes/Painel_testes.R
+++ R/Testes/Painel_testes.R
@@ -7,17 +7,6 @@ library(purrr)
 #tirando a notação científica
 options(scipen = 999)
 
-#iniciar paineis
-painel_1 = as.data.frame()
-painel_2 = as.data.frame()
-painel_3 = as.data.frame()
-painel_4 = as.data.frame()
-painel_5 = as.data.frame()
-painel_6 = as.data.frame()
-painel_7 = as.data.frame()
-painel_8 = as.data.frame()
-painel_9 = as.data.frame()
-
 #### loop para criar identificadores por painel por trimestre e salvar
 #fazendo um loop pra baixar cada trimestre da PNAD desde 2012
 pnad_list <- list() # create an empty list to store the data frames
@@ -35,7 +24,7 @@ for (i in years) {
       for(k in 1:9) {
       panel_list[[paste0("pnad", i, "_", j, "_", k)]] = vars_list[[paste0("vars", i, "_", j)]] %>%
         as.data.frame() %>%
-        dplyr::filter(V1014 == k) %>%
+        dplyr::filter(as.integer(V1014) == k) %>%
         cleans_dat() %>%
         builds_identifiers() %>%
         saveRDS(file = paste0(".\\pnad", i, "_", j, "_", k))
@@ -43,8 +32,28 @@ for (i in years) {
   }
 }
 
+painel_9 = rbind(readRDS(".\\pnad2021_1_9"), readRDS(".\\pnad2021_2_9"), readRDS(".\\pnad2021_3_9"), readRDS(".\\pnad2021_4_9"), readRDS(".\\pnad2022_1_9"), readRDS(".\\pnad2022_2_9"), readRDS(".\\pnad2022_3_9"), readRDS(".\\pnad2022_4_9"))
 
+x<- sort(table(painel_9$id_ind), decreasing= T)
+x<- data.frame(x)
+frequencia<- c()
+frequencia[1]<-filter(data.frame(x), Freq== 1)%>% nrow()
+frequencia[2]<-filter(data.frame(x), Freq== 2)%>% nrow()
+frequencia[3]<-filter(data.frame(x), Freq== 3)%>% nrow()
+frequencia[4]<-filter(data.frame(x), Freq== 4)%>% nrow()
+frequencia[5]<-filter(data.frame(x), Freq== 5)%>% nrow()
+frequencia[6]<-filter(data.frame(x), Freq== 6)%>% nrow()
+frequencia[7]<-filter(data.frame(x), Freq== 7)%>% nrow()
+frequencia[8]<-filter(data.frame(x), Freq== 8)%>% nrow()
+frequencia[9]<-filter(data.frame(x), Freq== 9)%>% nrow()
+frequencia[10]<-filter(data.frame(x), Freq== 10)%>% nrow()
+frequencia[11]<-filter(data.frame(x), Freq== 11)%>% nrow()
+frequencia[12]<-filter(data.frame(x), Freq== 12)%>% nrow()
 
+matriz<- data.frame(frequencia, contagem= seq(1:12))
+
+ggplot(data= matriz, mapping = aes(x= contagem, y= frequencia))+ 
+  geom_col()+geom_text(aes(label= frequencia), position= position_stack(vjust= 1.20), color= "red", size=4)
 
 
 #testando outro método
@@ -196,6 +205,4 @@ frequencia[12]<-filter(data.frame(x), Freq== 12)%>% nrow()
 matriz<- data.frame(frequencia, contagem= seq(1:12))
 
 ggplot(data= matriz, mapping = aes(x= contagem, y= frequencia))+ 
-  +   geom_col(fill= "black")+
-  geom_text(aes(label = frequencia), position = position_stack(vjust = 1.20 ), color= "red", size= 4)
-  
+  geom_col()
