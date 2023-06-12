b3885bded077837eb058f9c1f2019e2f6ddff6ca
Arthur Lins de Vasconcellos <tuca1211@gmail.com>
2023-06-12 16:18:27 -0300

    Minor changes- just some minor documentation

--- R/Testes/analise_estat_basico_met1.R
+++ R/Testes/analise_estat_basico_met1.R
@@ -1,13 +1,12 @@
 painel_6 = rbind(readRDS(".\\pnad2017_1_6"), readRDS(".\\pnad2017_2_6"), readRDS(".\\pnad2017_3_6"), readRDS(".\\pnad2017_4_6"), readRDS(".\\pnad2018_1_6"), readRDS(".\\pnad2018_2_6"), readRDS(".\\pnad2018_3_6"), readRDS(".\\pnad2018_4_6"), readRDS(".\\pnad2019_1_6")) %>%
   mutate(id_ind = as.character(id_ind))
-
-#atrito via individuo
-
+#atrito via individuo- exemplo usando o painel 6
 contagem_ind = table(painel_6$id_ind) %>% as.data.frame() %>%
   rename("id_ind" = "Var1", "contagem" = "Freq")
 
 com_contagem = left_join(painel_6, contagem_ind) %>%
-  dplyr::filter(V1016 == 1)
+  dplyr::filter(V1016 == 1) #filtrando para só contarmos as pessoas que fizeram a 1a entrevista
+#assim, medimos o real atrito, que é a quantidade de pessoas que fomos capazes de acompanhar ao longo das 5 entrevistas
 
 atrito = table(com_contagem$contagem) %>% as.data.frame() %>%
   rename("quantidades" = "Var1", "ocorrencias" = "Freq") %>%
