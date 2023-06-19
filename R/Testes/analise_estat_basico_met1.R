8dbe9022c372f40baa24b64583b1ddd876ab560f
Arthur Lins de Vasconcellos <tuca1211@gmail.com>
2023-06-19 18:50:09 -0300

    Criando as estatísticas de atrito para domicílios

--- R/Testes/analise_estat_basico_met1.R
+++ R/Testes/analise_estat_basico_met1.R
@@ -3,7 +3,6 @@ painel_6 = rbind(readRDS(".\\pnad2017_1_6"), readRDS(".\\pnad2017_2_6"), readRDS
 #atrito via individuo- exemplo usando o painel 6
 contagem_ind = table(painel_6$id_ind) %>% as.data.frame() %>%
   rename("id_ind" = "Var1", "contagem" = "Freq")
-
 com_contagem = left_join(painel_6, contagem_ind) %>%
   dplyr::filter(V1016 == 1) #filtrando para só contarmos as pessoas que fizeram a 1a entrevista
 #assim, medimos o real atrito, que é a quantidade de pessoas que fomos capazes de acompanhar ao longo das 5 entrevistas
@@ -15,16 +14,23 @@ atrito = table(com_contagem$contagem) %>% as.data.frame() %>%
 
 #atrito via domicilio
 
-contagem_dom = table(painel_6$id_dom) %>% as.data.frame() %>%
-  rename("id_dom" = "Var1", "contagem" = "Freq")
-
-com_contagem_dom = left_join(painel_6, contagem_dom) %>%
-  dplyr::filter(V1016 == 1)
-
-atrito_dom = table(com_contagem_dom$contagem) %>% as.data.frame() %>%
-  rename("quantidades" = "Var1", "ocorrencias" = "Freq") %>%
+#primeiro, vamos splitar o data frame de atrito em cada par (ano-trimestre) usando a função divide_trimestres, criada no doc "testes_basico_met1.R"
+painel_dividido<- divide_trimestres(painel_6)
+#agora criamos uma nova lista, com os valores únicos de id_dom de cada data frame da lista criada acima
+lista_unique<- list()
+for (i in 1:length(painel_dividido)) {
+  lista_unique[[i]] <-painel_dividido[[i]] %>% dplyr::pull(id_dom)%>% unique()%>% data.frame()
+}
+#criando uma coluna com todos ids únicos de cada trimestre do painel
+unique_id_dom<- unlist(lista_unique)%>% data.frame()
+unique_id_dom<-unique_id_dom |> rename("id_dom"=".")
+#obtendo as estatísticas para calcular o atrito
+contagem_dom = table(unique_id_dom$id_dom) %>% as.data.frame() %>%
+  rename("quantidades" = "Var1", "ocorrencias" = "Freq")%>%
   mutate(total = sum(ocorrencias)) %>%
   mutate(porcentagem = (ocorrencias/total)*100)
+#agrupando as observações pelo número de ocorrências, contamos cada uma tem e dividimos pelo total de domicílios únicos
+atrito_dom= contagem_dom |> group_by(ocorrencias) |> summarise(quantidade= n()/nrow(contagem_dom))
 
 #fazendo um loop para termos, em uma lista, um data frame com o atrito individual para cada painel
 painel_desejado<-data.frame()
