e545bfba2e1d69252b1e91a7e9c1ffc69b32c335
Arthur Lins de Vasconcellos <tuca1211@gmail.com>
2023-11-07 16:31:36 -0300

    Major changes to the advanced panel calculations

--- datazoom.social/R/PNADC/advanced panel functions.R
+++ datazoom.social/R/PNADC/advanced panel functions.R
@@ -41,7 +41,7 @@ data<- PNAD_painel_6_basic |> select(-c(id_dom,idind)) |> select(Ano, Trimestre,
                                                                  V3002A,V3003,V3003A,V3004,V3005,V3005A,V3006,V3006A,V3007,V3008,V3009,
                                                                  V3009A,V3010,V3011,V3011A, V3012,V3013,V3013A,V3013B,V3014)
 
-
+data_basic<- data |> basic_panel()
 #Now, we'll figure out a way to combine the persons that, if matched with other persons, would be seen in all the 5 interviews, through a slight modification of their id_ind
 
 # First, we'll have to line up the different changes available for each person
@@ -55,7 +55,7 @@ data<- PNAD_painel_6_basic |> select(-c(id_dom,idind)) |> select(Ano, Trimestre,
 #
 ###################################
 
-#1st level of strictness: if the observations match the specified criteria for this panel, they get the new id, which consists in:paste(V20081,V20082,V2003)
+#1st level of strictness: if the observations match the specified criteria for this panel, they get the new id, which consists in:paste(V20081,V2008,V2003)
 #criteria= either head of the house or their spouse
 #criteria 2= childs of the head of the house who are over 25 years old
 ##########################################
@@ -74,9 +74,9 @@ data_joined<- left_join(data_basic, summary_data, by= "id_ind")
 # creating the column for the 1st stage id, which is created only to those who were not fully matched and match the criteria below
 dataframe_1st_stage <- data_joined %>%
   mutate(id_1st_stage = ifelse(V2005 %in% c("1", "2", "3") & missing_quarters!= "" | (V2005 %in% c("4", "5") & as.numeric(V2009) >= 25 & missing_quarters!= ""),
-                               paste(V20081, V20082, V2003),
+                               paste(V20081, V2008, V2003),
                                id_ind))
-#now, we can calculate the freaking friction, thank goodness
+#now, we can calculate the friction
 #pulling the people who appeared in the 1st quarter
 first_quarter_appearance<- dataframe_1st_stage |> filter(V1016== "1") |> pull(id_1st_stage) |> as.vector()
 #first, let's calculate the number of answers by quarter
@@ -85,7 +85,8 @@ summary_appearances<- dataframe_1st_stage |> filter(id_1st_stage %in% first_quar
   group_by(id_1st_stage) |> summarize(appearances = list(V1016), #in this way, we can "paste" in a single line the interviews the person has appeared at
                                       disappearances = list(setdiff(1:5, unique(V1016)))) %>% #and then we can set the difference from the 1:5 vector, which will return the interviews not attended by the each one
   rowwise() %>% #perform the next commands line by line (it was doing the "unlist()" command to ALL observations of the "disappearances" column)
-  mutate(first_interview= ifelse("1" %in% unlist(disappearances), 1, 0),
+  mutate(missing_quarters= paste(as.character(unlist(disappearances)), collapse = " "),
+         first_interview= ifelse("1" %in% unlist(disappearances), 1, 0),
          second_interview= ifelse("2" %in% unlist(disappearances), 1,0),
          third_interview= ifelse("3" %in% unlist(disappearances), 1, 0),
          fourt_interview= ifelse("4" %in% unlist(disappearances), 1, 0),
@@ -111,7 +112,111 @@ colnames(comparison_df)[1:3] <- paste0("advanced_panel_level_1_", colnames(compa
 colnames(comparison_df)[4:6] <- paste0("basic_panel_", colnames(comparison_df)[4:6])
 
 writexl::write_xlsx(comparison_df, "C:/Users/tuca1/OneDrive/Documentos/Datazoom/Painel_PNAD/paineis_novos_advanced/atrito/comparacao_atrito_painel_6.xlsx")
+
+###########################################################
+#turning this into a function
+
+panel_advanced_1st_level<- function(data){
+  data_initial<- data |> select(-c(id_dom,idind)) |> select(Ano, Trimestre,UF, Capital, UPA, Estrato, V1008,V1014,V1016,V1022,V1023,
+                                                                   V2001,V2003,V2005,V2007, V2008,V20081,V20082,V2009,V2010,V3001,V3002,
+                                                                   V3002A,V3003,V3003A,V3004,V3005,V3005A,V3006,V3006A,V3007,V3008,V3009,
+                                                                   V3009A,V3010,V3011,V3011A, V3012,V3013,V3013A,V3013B,V3014)
+  data_basic_panel<- data_initial |> basic_panel()
+  summary_data <- data_basic_panel %>%
+    group_by(id_ind) %>% #grouping by each individual
+    summarize(appearances = list(V1016), #in this way, we can "paste" in a single line the interviews the person has appeared at
+              disappearances = list(setdiff(1:5, unique(V1016)))) %>% #and then we can set the difference from the 1:5 vector, which will return the interviews not attended by the each one
+    rowwise() %>% #perform the next commands line by line (it was doing the "unlist()" command to ALL observations of the "disappearances" column)
+    mutate(missing_quarters = paste(as.character(unlist(disappearances)), collapse = " ")) #getting a column that tells us in which interviews was each person missing
+  #rejoining this dataframe with our original database via left_join
+
+  data_joined<- left_join(data_basic_panel, summary_data, by= "id_ind")
+
+  # creating the column for the 1st stage id, which is created only to those who were not fully matched and match the criteria below
+  dataframe_1st_stage <- data_joined %>%
+    mutate(id_1st_stage = ifelse(V2005 %in% c("1", "2", "3") & missing_quarters!= "" | (V2005 %in% c("4", "5") & as.numeric(V2009) >= 25 & missing_quarters!= ""),
+                                 paste(V20081, V2008, V2003),
+                                 id_ind))
+  #now, we can calculate the freaking friction, thank goodness
+  #pulling the people who appeared in the 1st quarter
+  first_quarter_appearance<- dataframe_1st_stage |> filter(V1016== "1") |> pull(id_1st_stage) |> as.vector()
+  #first, let's calculate the number of answers by quarter
+
+  summary_appearances<- dataframe_1st_stage |> filter(id_1st_stage %in% first_quarter_appearance) |>
+    group_by(id_1st_stage) |> summarize(appearances = list(V1016), #in this way, we can "paste" in a single line the interviews the person has appeared at
+                                        disappearances = list(setdiff(1:5, unique(V1016)))) %>% #and then we can set the difference from the 1:5 vector, which will return the interviews not attended by the each one
+    rowwise() %>% #perform the next commands line by line (it was doing the "unlist()" command to ALL observations of the "disappearances" column)
+    mutate(first_interview= ifelse("1" %in% unlist(disappearances), 1, 0),
+           second_interview= ifelse("2" %in% unlist(disappearances), 1,0),
+           third_interview= ifelse("3" %in% unlist(disappearances), 1, 0),
+           fourt_interview= ifelse("4" %in% unlist(disappearances), 1, 0),
+           fifth_interview= ifelse("5" %in% unlist(disappearances), 1, 0))
+
+  #then, using this dataframe, we'll generate one that calculate the friction
+  atrito_definite= data.frame(Entrevista=seq(1,5),"Contagem de faltantes"= c(0,0,0,0,0))
+  for (i in 4:ncol(summary_appearances)) {
+    atrito_definite[i-3,2]<- sum(summary_appearances[,i])# tiramos 4 pois queremos adicionar às 5 primeiras colunas de atrito_definite os dados das colunas 5:8 de summary_data
+  }
+
+  atrito_definite$Percentage_found= 100*(round(1-(atrito_definite$Contagem.de.faltantes/nrow(summary_appearances)),5))# nrow(summary_appearances)= Number of responses
+  # atrito_definite$Contagem.de.faltantes= Contagem de faltantes por entrevista.
+return(atrito_definite)
+}
+
+#calculating the basic panel's friction rate
+oioi_atrito_painel_6<-cria_df_de_atrito(data_basic)
+#calculating the advanced panel's friction rate
+atrito_avancado<- panel_advanced_1st_level(data_basic)
+#generating the comparison between the basic and the advanced panels for panel 6
+
+comparison_df<- cbind(atrito_definite, oioi_atrito_painel_6)
+# Assuming your dataframe is named "df"
+colnames(comparison_df)[1:3] <- paste0("advanced_panel_level_1_", colnames(comparison_df)[1:3])
+colnames(comparison_df)[4:6] <- paste0("basic_panel_", colnames(comparison_df)[4:6])
+
+##############################################################
+#2nd level of strictness (column 3 at the Ribas and Soares article:if the observations match the specified criteria for this panel, they get the new id, which consists in:paste(V2007,V20081+-2,V2008+-4,V2003)
+#criteria= either head of the house or their spouse
+#criteria 2= childs of the head of the house who are over 25 years old
 ##########################################
+
+#first, we'll filter the dataframe to get only the people who have not been matched fully yet, considering both basic and advanced panels (the past criterias, of course)
+
+appearances_2nd_phase_ids<- summary_appearances |> filter(missing_quarters!= "") |> pull(id_1st_stage)
+#here we have 60327 individuals not identified fully yet
+
+#now filtering the dataframe to have only these people who have not been fully identified
+df_2nd_phase<- dataframe_1st_stage |> mutate(id_2nd_stage= paste(V2003,V2007,V2008,V20081)) |> filter(id_1st_stage %in% appearances_2nd_phase_ids)
+#now, creating a function that matches people inside every household
+
+#first, dividing the df into a list of dataframes, one per household, to facilitate the function we'll do next
+list_doms_2nd_phase<- split(df_2nd_phase,df_2nd_phase$id_dom)
+
+group_similar <- function(df) {
+  df_summarised<- df |> group_by(id_2nd_stage) |> summarise(Mes_nascimento= as.numeric(V20081), dia_nascimento=as.numeric(V2008),mini_id= paste(V2003,V2007))
+  for (i in 1:nrow(df_summarised)) {
+    for (j in 1:nrow(df_summarised)) {
+      if (i != j) {
+        if (abs(df_summarised$Mes_nascimento[i] - df_summarised$Mes_nascimento[j]) <= 2 &&
+            abs(df_summarised$dia_nascimento[i] - df_summarised$dia_nascimento[j]) <= 4 &&
+            df_summarised$mini_id[i]== df_summarised$mini_id[j]) {
+          df_summarised$id_2nd_stage[i] <- df_summarised$id_2nd_stage[j]
+        }
+      }
+    }
+  }
+  return(df_summarised)
+}
+
+
+# Apply the grouping function to each household
+matched_households <- lapply(list_doms_2nd_phase, group_similar)
+
+# Combine the grouped households back into a single dataframe
+df_grouped <-rbindlist(grouped_households)
+
+
+
 
 # From this spot on, you can ignore my code, it is just me going insane trying to create perfect matches in a way that will make sense in the future, just not now
 ############### IGNORE THIS IGNORE THIS, IGNORE THIS ##################################
@@ -142,7 +247,7 @@ data_splited<-split(df_1_to_5_stages_factorized,f= df_1_to_5_stages_factorized$m
 # Generating a new id (a more relaxed one to all the observations), which is composed only by the day and month of birth, and the control number
 
 gen_1st_stage_id<- function(data){
-  data_transformed<- data |> mutate(id_1st_stage= paste(V20081,V20082,V2003)) |>
+  data_transformed<- data |> mutate(id_1st_stage= paste(V20081,V2008,V2003)) |>
     mutate(appearances_character = paste(as.character(unlist(appearances)), collapse = " ")) }
 list_1st_stage<-lapply(data_splited, gen_1st_stage_id)
 
@@ -188,6 +293,6 @@ names(lista_matching_obs)= nomes_lista
 ############### IGNORE THIS IGNORE THIS, IGNORE THIS ############################################################################################################################
 #Now, for the 1st stage, we'll calculate the friction with for the basic id_ind and the new, advanced, individual id, for every group, as to evaluate the impact of this relaxation
 
-############### IGNORE THIS IGNORE THIS, IGNORE THIS ############################################################################################################################
+
 
 
