655ea491c911363016cb7e730a0bdd0d9e9abe15
ArthurLins-code <tuca1211@gmail.com>
2024-01-11 11:42:24 -0300

    Deleting unnecessary code- turning the advanced panel into functions

--- datazoom.social/R/PNADC/advanced panel functions.R
+++ datazoom.social/R/PNADC/advanced panel functions.R
@@ -2,8 +2,6 @@
 ###               Build advanced PNADC panel
 ### This file contains PNADC advanced panel related functions
 ##########################
-setwd("C:/Users/tuca1/OneDrive/Documentos/GitHub/Datazoom-Social_R/datazoom.social/R/PNADC")
-
 library(haven)
 library(tidyverse)
 library(ggplot2)
@@ -59,122 +57,17 @@ data_basic<- data |> basic_panel()
 #1st level of strictness: if the observations match the specified criteria for this panel, they get the new id, which consists in:paste(V20081,V2008,V2003)
 #criteria= either head of the house or their spouse
 #criteria 2= childs of the head of the house who are over 25 years old
-##########################################
-
-
-summary_data <- data_basic %>%
-  group_by(id_ind) %>% #grouping by each individual
-  summarize(appearances = list(V1016), #in this way, we can "paste" in a single line the interviews the person has appeared at
-            disappearances = list(setdiff(1:5, unique(V1016)))) %>% #and then we can set the difference from the 1:5 vector, which will return the interviews not attended by the each one
-  rowwise() %>% #perform the next commands line by line (it was doing the "unlist()" command to ALL observations of the "disappearances" column)
-  mutate(missing_quarters = paste(as.character(unlist(disappearances)), collapse = " ")) #getting a column that tells us in which interviews was each person missing
-#rejoining this dataframe with our original database via left_join
-
-data_joined<- left_join(data_basic, summary_data, by= "id_ind")
-
-# creating the column for the 1st stage id, which is created only to those who were not fully matched and match the criteria below
-dataframe_1st_stage <- data_joined %>%
-  mutate(id_1st_stage = ifelse(V2005 %in% c("1", "2", "3") & missing_quarters!= "" | (V2005 %in% c("4", "5") & as.numeric(V2009) >= 25 & missing_quarters!= ""),
-                               paste(V20081, V2008, V2003),
-                               id_ind))
-#now, we can calculate the friction
-#pulling the people who appeared in the 1st quarter
-first_quarter_appearance<- dataframe_1st_stage |> filter(V1016== "1") |> pull(id_1st_stage) |> as.vector()
-#first, let's calculate the number of answers by quarter
-
-summary_appearances<- dataframe_1st_stage |> filter(id_1st_stage %in% first_quarter_appearance) |>
-  group_by(id_1st_stage) |> summarize(appearances = list(V1016), #in this way, we can "paste" in a single line the interviews the person has appeared at
-                                      disappearances = list(setdiff(1:5, unique(V1016)))) %>% #and then we can set the difference from the 1:5 vector, which will return the interviews not attended by the each one
-  rowwise() %>% #perform the next commands line by line (it was doing the "unlist()" command to ALL observations of the "disappearances" column)
-  mutate(missing_quarters= paste(as.character(unlist(disappearances)), collapse = " "),
-         first_interview= ifelse("1" %in% unlist(disappearances), 1, 0),
-         second_interview= ifelse("2" %in% unlist(disappearances), 1,0),
-         third_interview= ifelse("3" %in% unlist(disappearances), 1, 0),
-         fourt_interview= ifelse("4" %in% unlist(disappearances), 1, 0),
-         fifth_interview= ifelse("5" %in% unlist(disappearances), 1, 0))
-
-#then, using this dataframe, we'll generate one that calculate the friction
-atrito_definite= data.frame(Entrevista=seq(1,5),"Contagem de faltantes"= c(0,0,0,0,0))
-for (i in 4:ncol(summary_appearances)) {
-  atrito_definite[i-3,2]<- sum(summary_appearances[,i])# tiramos 4 pois queremos adicionar às 5 primeiras colunas de atrito_definite os dados das colunas 5:8 de summary_data
-}
-
-atrito_definite$Percentage_found= 100*(round(1-(atrito_definite$Contagem.de.faltantes/nrow(summary_appearances)),5))# nrow(summary_appearances)= Number of responses
-# atrito_definite$Contagem.de.faltantes= Contagem de faltantes por entrevista.
-
-#calculating the basic panel's friction rate
-oioi_atrito_painel_6<-cria_df_de_atrito(data_basic)
-
-#generating the comparison between the basic and the advanced panels for panel 6
-
-comparison_df<- cbind(atrito_definite, oioi_atrito_painel_6)
-# Assuming your dataframe is named "df"
-colnames(comparison_df)[1:3] <- paste0("advanced_panel_level_1_", colnames(comparison_df)[1:3])
-colnames(comparison_df)[4:6] <- paste0("basic_panel_", colnames(comparison_df)[4:6])
-
-writexl::write_xlsx(comparison_df, "C:/Users/tuca1/OneDrive/Documentos/Datazoom/Painel_PNAD/paineis_novos_advanced/atrito/comparacao_atrito_painel_6.xlsx")
-
 ###########################################################
 #turning this into a function
 
 panel_advanced_1st_level<- function(data){
-  data_initial<- data |> select(-c(id_dom,idind)) |> select(Ano, Trimestre,UF, Capital, UPA, Estrato, V1008,V1014,V1016,V1022,V1023,
-                                                                   V2001,V2003,V2005,V2007, V2008,V20081,V20082,V2009,V2010,V3001,V3002,
-                                                                   V3002A,V3003,V3003A,V3004,V3005,V3005A,V3006,V3006A,V3007,V3008,V3009,
-                                                                   V3009A,V3010,V3011,V3011A, V3012,V3013,V3013A,V3013B,V3014)
-  data_basic_panel<- data_initial |> basic_panel()
-  summary_data <- data_basic_panel %>%
-    group_by(id_ind) %>% #grouping by each individual
-    summarize(appearances = list(V1016), #in this way, we can "paste" in a single line the interviews the person has appeared at
-              disappearances = list(setdiff(1:5, unique(V1016)))) %>% #and then we can set the difference from the 1:5 vector, which will return the interviews not attended by the each one
-    rowwise() %>% #perform the next commands line by line (it was doing the "unlist()" command to ALL observations of the "disappearances" column)
-    mutate(missing_quarters = paste(as.character(unlist(disappearances)), collapse = " ")) #getting a column that tells us in which interviews was each person missing
-  #rejoining this dataframe with our original database via left_join
-
-  data_joined<- left_join(data_basic_panel, summary_data, by= "id_ind")
-
   # creating the column for the 1st stage id, which is created only to those who were not fully matched and match the criteria below
   dataframe_1st_stage <- data_joined %>%
     mutate(id_1st_stage = ifelse(V2005 %in% c("1", "2", "3") & missing_quarters!= "" | (V2005 %in% c("4", "5") & as.numeric(V2009) >= 25 & missing_quarters!= ""),
                                  paste(V20081, V2008, V2003),
                                  id_ind))
-  #now, we can calculate the freaking friction, thank goodness
-  #pulling the people who appeared in the 1st quarter
-  first_quarter_appearance<- dataframe_1st_stage |> filter(V1016== "1") |> pull(id_1st_stage) |> as.vector()
-  #first, let's calculate the number of answers by quarter
-
-  summary_appearances<- dataframe_1st_stage |> filter(id_1st_stage %in% first_quarter_appearance) |>
-    group_by(id_1st_stage) |> summarize(appearances = list(V1016), #in this way, we can "paste" in a single line the interviews the person has appeared at
-                                        disappearances = list(setdiff(1:5, unique(V1016)))) %>% #and then we can set the difference from the 1:5 vector, which will return the interviews not attended by the each one
-    rowwise() %>% #perform the next commands line by line (it was doing the "unlist()" command to ALL observations of the "disappearances" column)
-    mutate(first_interview= ifelse("1" %in% unlist(disappearances), 1, 0),
-           second_interview= ifelse("2" %in% unlist(disappearances), 1,0),
-           third_interview= ifelse("3" %in% unlist(disappearances), 1, 0),
-           fourt_interview= ifelse("4" %in% unlist(disappearances), 1, 0),
-           fifth_interview= ifelse("5" %in% unlist(disappearances), 1, 0))
-
-  #then, using this dataframe, we'll generate one that calculate the friction
-  atrito_definite= data.frame(Entrevista=seq(1,5),"Contagem de faltantes"= c(0,0,0,0,0))
-  for (i in 4:ncol(summary_appearances)) {
-    atrito_definite[i-3,2]<- sum(summary_appearances[,i])# tiramos 4 pois queremos adicionar às 5 primeiras colunas de atrito_definite os dados das colunas 5:8 de summary_data
-  }
-
-  atrito_definite$Percentage_found= 100*(round(1-(atrito_definite$Contagem.de.faltantes/nrow(summary_appearances)),5))# nrow(summary_appearances)= Number of responses
-  # atrito_definite$Contagem.de.faltantes= Contagem de faltantes por entrevista.
-return(atrito_definite)
+  return(dataframe_1st_stage)
 }
-
-#calculating the basic panel's friction rate
-oioi_atrito_painel_6<-cria_df_de_atrito(data_basic)
-#calculating the advanced panel's friction rate
-atrito_avancado<- panel_advanced_1st_level(data_basic)
-#generating the comparison between the basic and the advanced panels for panel 6
-
-comparison_df<- cbind(atrito_definite, oioi_atrito_painel_6)
-# Assuming your dataframe is named "df"
-colnames(comparison_df)[1:3] <- paste0("advanced_panel_level_1_", colnames(comparison_df)[1:3])
-colnames(comparison_df)[4:6] <- paste0("basic_panel_", colnames(comparison_df)[4:6])
-
 ##############################################################
 #2nd level of strictness (column 3 at the Ribas and Soares article:if the observations match the specified criteria for this panel, they get the new id, which consists in:paste(V2007,V20081+-2,V2008+-4,V2003)
 #criteria= either head of the house or their spouse
