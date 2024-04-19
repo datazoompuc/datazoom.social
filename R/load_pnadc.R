be60fafaf4db836c728147f4d5a7645326b761fe
Igor Rigolon <igor.rilave@hotmail.com>
2024-04-19 19:58:06 -0300

    fixing type mismatch

--- R/load_pnadc.R
+++ R/load_pnadc.R
@@ -538,7 +538,7 @@ treat_pnadc <- function(df) {
       ),
       cnae_2dig = dplyr::case_match(
         V4013,
-        c(paste0(0, 1201:1209), paste0(0, 1402:1409), "01999") ~ "Pecu\u00e1ria e cria\u00e7\u00e3o de animais",
+        as.numeric(c(paste0(0, 1201:1209), paste0(0, 1402:1409), "01999")) ~ "Pecu\u00e1ria e cria\u00e7\u00e3o de animais",
         .default = cnae_2dig
       )
     )
