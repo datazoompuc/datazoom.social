cd04660a4b337c59d360e1817ca774a29b485fd1
Laura T. Regadas <lauratregadas@gmail.com>
2024-04-22 20:08:52 -0300

    remove unnecessary code in load_pnadc
    could cause conflict with master. Igor already solved issue #55

--- R/load_pnadc.R
+++ R/load_pnadc.R
@@ -537,7 +537,7 @@ treat_pnadc <- function(df) {
         "99" ~ "Outros"
       ),
       cnae_2dig = dplyr::case_match(
-        as.numeric(V4013),
+        V4013,
         c(paste0(0, 1201:1209), paste0(0, 1402:1409), "01999") ~ "Pecu\u00e1ria e cria\u00e7\u00e3o de animais",
         .default = cnae_2dig
       )
