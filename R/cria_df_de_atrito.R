a985fa8c27599e954e7c7d08c60b1b822fc3a9fc
Bernardo Furlanetto Sieira <bernardo.sieira@gmail.com>
2026-05-31 01:41:13 -0300

    avoiding roxygen compiling

--- R/cria_df_de_atrito.R
+++ R/cria_df_de_atrito.R
@@ -3,10 +3,10 @@
 #' This function generates a summary dataframe indicating the count of missing
 #' interviews for each individual and the unconditional tracking rates.
 #'
-#' @param data The input data frame, preferably a PNADc panel file.
-#' @param panel The identification strategy: "basic", "advanced_1", "advanced_2", "advanced_3" or "households".
+#' data: The input data frame, preferably a PNADc panel file.
+#' panel: The identification strategy: "basic", "advanced_1", "advanced_2", "advanced_3" or "households".
 #'
-#' @return A data frame summarizing missing interviews and the tracking rates.
+#' A data frame summarizing missing interviews and the tracking rates.
 cria_df_de_atrito <- function(data, panel) {
   # Binding globals
   V1016 <- individual_identifier <- disappearances <- NULL
